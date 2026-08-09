2e807463e1a1170dd379ce214376f1cd1b19e4e5
LeLyPhucAn <phucan20032011@gmail.com>
2026-08-10 01:02:01 +0700

    feat(server): complete TCP server core and client connection test

---
+++ Code/Server/Managers/ConnectionManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Server.Network;
+
+namespace Server.Managers;
+
+public class ConnectionManager
+{
+    private readonly List<ClientSession> _clients = new();
+
+    public void Add(ClientSession session)
+    {
+        _clients.Add(session);
+
+        Console.WriteLine($"[+] Client connected ({_clients.Count})");
+    }
+
+    public void Remove(ClientSession session)
+    {
+        _clients.Remove(session);
+
+        Console.WriteLine($"[-] Client disconnected ({_clients.Count})");
+    }
+
+    public IReadOnlyList<ClientSession> GetAll()
+    {
+        return _clients.AsReadOnly();
+    }
+
+    public int Count => _clients.Count;
+}
