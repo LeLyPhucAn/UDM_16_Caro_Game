5e6793e7ddcd474e5c773aee14e7bc032fdf15e8
LeLyPhucAn <phucan20032011@gmail.com>
2026-08-17 22:22:51 +0700

    fix(database): sua loi namespace, duplicate DatabaseHelper, them ham Login

--- Code/Server/Repositories/MatchRepository.cs
+++ Code/Server/Repositories/MatchRepository.cs
@@ -1,13 +1,26 @@
+using System.Data;
+using Microsoft.Data.SqlClient;
 using Server.Database;
 
 namespace Server.Repositories;
 
 public class MatchRepository
 {
-    private readonly DatabaseHelper _dbHelper;
-
-    public MatchRepository(DatabaseHelper dbHelper)
+    public DataTable GetMatchHistory(int userId)
     {
-        _dbHelper = dbHelper;
+        string query = "SELECT * FROM Matches WHERE Player1ID = @UserId OR Player2ID = @UserId";
+        SqlParameter[] parameters = { new SqlParameter("@UserId", userId) };
+        return DatabaseHelper.ExecuteQuery(query, parameters);
+    }
+
+    public int SaveMatch(int player1Id, int player2Id, int winnerId)
+    {
+        string query = "INSERT INTO Matches (Player1ID, Player2ID, WinnerID, MatchDate) VALUES (@P1, @P2, @Winner, GETDATE())";
+        SqlParameter[] parameters = {
+            new SqlParameter("@P1", player1Id),
+            new SqlParameter("@P2", player2Id),
+            new SqlParameter("@Winner", winnerId)
+        };
+        return DatabaseHelper.ExecuteNonQuery(query, parameters);
     }
 }
