6808cc75e1530508ec874e59ef485ef99ea754c0
lekiet78 <anhhunglahay@gmail.com>
2026-08-30 22:41:59 +0700

    feat: complete task 2 game timer and result

---
+++ Code/Shared/Enums/GameResultType.cs
@@ -0,0 +1,28 @@
+﻿namespace Shared.Enums
+{
+    /// <summary>
+    /// Loại kết quả của một trận Caro.
+    /// </summary>
+    public enum GameResultType
+    {
+        /// <summary>
+        /// Một người chơi thắng bằng cách tạo đủ 5 quân liên tiếp.
+        /// </summary>
+        Win,
+
+        /// <summary>
+        /// Hai người chơi hòa vì bàn cờ đã đầy.
+        /// </summary>
+        Draw,
+
+        /// <summary>
+        /// Người chơi hết thời gian.
+        /// </summary>
+        Timeout,
+
+        /// <summary>
+        /// Trận đấu bị kết thúc bởi Server hoặc người chơi rời trận.
+        /// </summary>
+        Abandoned
+    }
+}
